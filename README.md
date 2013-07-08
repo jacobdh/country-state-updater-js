6209848a0033431adec81b58aba364d2d1ceafb6
Jake Heaton <jake.heaton@franklincovey.com>
2013-07-08 13:46:22 -0600

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-country-state-js
+Country/State Updater JavaScript Class
 ================
+
+This is a simple and lightweight JavaScript class for dependent country/state dropdowns.
+You select a country, and the states are updated.
+
+This class has no external dependencies, and you can use your own country data or the default data provided in country-state-updater-data.js.
---
+++ js/country-state-updater.js
@@ -0,0 +1,76 @@
+var CountryStateUpdater = CountryStateUpdater || {};
+CountryStateUpdater = function(options){
+    options = options || {};
+    var countryElem = getElem(options.countryElem);
+    var stateElem = getElem(options.stateElem);
+    var noCountriesOption = options.noCountriesOption || 'None / Not Applicable';
+    var defaultCountry = options.defaultCountry !== undefined ? options.defaultCountry : 'US';
+    var defaultState = options.defaultState || null;
+    var countries = options.countries || (CountryStateUpdaterData && CountryStateUpdaterData.countries ? CountryStateUpdaterData.countries : {});
+    var states = options.states || (CountryStateUpdaterData && CountryStateUpdaterData.states ? CountryStateUpdaterData.states : {});
+
+    init();
+    
+    function init() {
+        if (!countryElem || !stateElem) {
+            return;
+        }
+
+        for (var countryCode in countries) {
+           var optionElem = document.createElement('option');
+           optionElem.value = countryCode;
+           optionElem.innerHTML = countries[countryCode];
+           countryElem.appendChild(optionElem);
+        }
+
+        if (defaultCountry && countries[defaultCountry]) {
+            countryElem.value = defaultCountry;
+            updateStates();
+
+            if (defaultState && states[defaultCountry][defaultState]) {
+                stateElem.value = defaultState;
+            }
+        }
+
+        addEvent(countryElem, 'change', updateStates);
+    }
+
+    function updateStates() {
+        for (var i = stateElem.options.length-1; i >= 0; i--) {
+            if (i > 0 || stateElem.options[i].value) {
+                stateElem.remove(i);
+            }
+        }
+
+        var newStates = countryElem.value && states && states[countryElem.value] ? states[countryElem.value] : {'':noCountriesOption};
+        for (var stateCode in newStates) {
+            var optionElem = document.createElement('option');
+            optionElem.value = stateCode;
+            optionElem.innerHTML = newStates[stateCode];
+            stateElem.appendChild(optionElem);
+        }
+    }
+
+    function getElem(x) {
+        if (!x) {
+            return null;
+        } else if (typeof x === 'string') {
+            return document.getElementById(x);
+        }
+
+        return x;
+    }
+
+    // From John Resig: http://ejohn.org/blog/flexible-javascript-events/
+    function addEvent(obj, type, fn) {
+        if (obj.attachEvent) {
+            obj['e'+type+fn] = fn;
+            obj[type+fn] = function(){
+                obj['e'+type+fn](window.event);
+            };
+            obj.attachEvent('on'+type, obj[type+fn]);
+        } else {
+            obj.addEventListener(type, fn, false);
+        }
+    }
+};
